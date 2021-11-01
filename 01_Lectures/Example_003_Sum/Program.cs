e73ce55ecf43425149e8ef65e571de0feceb224c
Ryabov Dmitry <dimir996@gmail.com>
2021-11-01 12:53:45 +0100

    Add Random grnerator

--- 01_Lectures/Example_003_Sum/Program.cs
+++ 01_Lectures/Example_003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); // Random int 0 to 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,20);  // Random int 0 to 19
+Console.WriteLine(numberB);
 int result = numberA+numberB;
 Console.WriteLine(result);
 
