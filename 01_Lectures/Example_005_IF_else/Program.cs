03df6ed5069e16f648e05db7345080096caed1a9
Ryabov Dmitry <dimir996@gmail.com>
2021-11-01 13:13:01 +0100

    Add if else example

---
+++ 01_Lectures/Example_005_IF_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower == "Liza")
+{
+    Console.WriteLine("Ура, это же Лиза!!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+
+}
