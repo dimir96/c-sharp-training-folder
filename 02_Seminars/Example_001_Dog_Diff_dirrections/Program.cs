32a521386b1bc7356081be32c666c20524c1eb00
Ryabov Dmitry <dimir996@gmail.com>
2021-11-03 00:10:52 +0100

    added dog in different directions

---
+++ 02_Seminars/Example_001_Dog_Diff_dirrections/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задачка про друзей в разные стороны 
+
+// count = 0, distance = 10 000 firstFriendSpeed = 5 secondFriendSpeed = 2 dogSpeed = 10 friend =2 mittingDistance = 1
+
+// Получаем переменные 
+int count = 0;
+int mittingDistance = 5;
+double time = 0;
+Console.Write("Пожалуйста введите расстояние между друзьями [м]: ");
+double distance = Convert.ToInt32(Console.ReadLine());
+Console.Write("Пожалуйста введите скорость первого друга [м/с]: ");
+int firstFriendSpeed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Пожалуйста введите скорость второго друга [м/с]: ");
+int secondFriendSpeed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Пожалуйста скорость собаки [м/с]: ");
+int dogSpeed = Convert.ToInt32(Console.ReadLine());
+
+// Расчеты
+while (distance>mittingDistance)
+{
+    
+    if ( count % 2 == 0)
+    {
+        time = distance / (dogSpeed + secondFriendSpeed);
+        count++;
+        Console.WriteLine(distance);
+    }
+    else
+    {
+        time = distance / (dogSpeed + firstFriendSpeed);
+        count++;
+        Console.WriteLine(distance);
+    }
+
+
+    distance = distance - time * (firstFriendSpeed + secondFriendSpeed);
+}
+
+// Выводим значение
+
+Console.Write("Собака пробежит: ");
+Console.Write(count);
+Console.Write(" раз");
