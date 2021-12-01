8d0e7bec3927f381297a119bd924f48bb2268eef
Ryabov Dmitry <dimir996@gmail.com>
2021-12-01 01:03:14 +0100

    Added task04

--- 02_Seminars/Task03_ProfCode/Program.cs
+++ 02_Seminars/Task03_ProfCode/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// Задача 3
+// Написать программу, вычисляющую значение периодической функции f(x), в произвольной точке x, если на промежутке [a;b], который составляет один её период, значения функции вычисляются по формуле
+
+// [-3; 3], f(x) = |x| - 1
+// [-10; -2], f(x) = 1/x + 0.6
+// [-4; 3], f(x) = Cos(|π*x|)
+double Random (int min, int max)
+    {
+       int number = new Random().Next(min,max+1);
+       Console.WriteLine($"Искомый x: {number}");
+       
+       return number;
+    }
+
+// [-3; 3], f(x) = |x| - 1
+
+double x1 = Random(-3,3);
+double result1 = Math.Abs(x1) - 1;
+Console.WriteLine($"Результат f({x1}) = |{x1}| - 1 = {result1}");
+
+Console.WriteLine();
+
+// [-10; -2], f(x) = 1/x + 0.6
+double x2 = Random(-10,-2);
+double result2 = (1/x2) + 0.6;
+Console.WriteLine($"Результат f({x2}) = 1/{x2} + 0.6 = {result2}");
+
+Console.WriteLine();
+
+// [-4; 3], f(x) = Cos(|π*x|)
+double x3 = Random(-10,-2);
+double result3 = Math.Cos(Math.Abs(Math.PI*x3));
+Console.WriteLine($"Результат f({x3}) = Cos(|π*{x3}|) = {result3}");
+
+// ИСПРАВИТЬ ЗАДАЧИ 
---
+++ 02_Seminars/Task04_ProfCode/Program.cs
@@ -0,0 +1,67 @@
+﻿Console.Clear();
+
+// Задача 4
+// Даны координаты концов двух отрезков [a; b], [c; d]. Верно ли, что
+
+// отрезки не пересекаются
+// отрезки пересекаются строго в одной точке
+// один отрезок вложен в другой
+
+void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    }
+int[] CreateArray (int min, int max)
+    {
+       int[] array = new int[max-min+1]; 
+       array[0]=min;
+        for (int i = 1; i < max-min+1; i++)
+        {
+            array [i]=min+i;
+        }
+        return array;
+    }
+
+
+int[] array1 = CreateArray(2,3);
+int[] array2 = CreateArray(1,5);
+
+
+PrintArray(array1);
+Console.WriteLine();
+PrintArray(array2);
+Console.WriteLine();
+
+// отрезки не пересекаются
+
+bool flag = true;
+if(array1[0] > array2[array2.Length-1] | array1[array1.Length-1] < array2[0]) flag = false;
+if (flag == false) Console.WriteLine("1 -- Отрезки не пересекаются");
+
+else
+Console.WriteLine("1 -- Отрезки пересекаются");
+
+
+Console.WriteLine();
+
+// отрезки пересекаются строго в одной точке
+
+flag = false;
+if(array1[0]== array2[array2.Length-1] | array2[0]== array1[array1.Length-1]) flag = true;
+if (flag == true) Console.WriteLine("2 -- Oтрезки пересекаются строго в одной точке");
+else
+Console.WriteLine("1 -- Oтрезки НЕ пересекаются строго в одной точке");
+
+Console.WriteLine();
+
+// один отрезок вложен в другой
+
+flag = false;
+if( (array1[0] > array2[0] & array1[array1.Length-1] < array2[array2.Length-1]) | 
+    (array1[0] < array2[0] & array1[array1.Length-1] > array2[array2.Length-1])) flag = true;
+if (flag == true) Console.WriteLine("3 -- Один отрезок вложен в другой");
+else
+Console.WriteLine("3 -- Один отрезок НЕ вложен в другой");
